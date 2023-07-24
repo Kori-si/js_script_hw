a1723ca98854c40cb7f23c37db8fdfd984b48f64
Korinna Sinelnikova <103421831+Kori-si@users.noreply.github.com>
2023-07-24 16:12:36 +0200

    Add files via upload

---
+++ hw_1/script.js
@@ -0,0 +1,9 @@
+// задание 1
+let celsius = 50;
+let fahrenheit = (9/5)*celsius+32;
+console.log(`Температура в градусах цельсия: ${celsius}, температура по фаренгейту: ${fahrenheit}`);
+
+// задание 2
+const nameN = 'Kori';
+let admin = nameN;
+console.log(admin);
